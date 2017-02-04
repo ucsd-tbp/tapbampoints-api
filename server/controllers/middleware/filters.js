56505c3c0ac5f915d249489a3d6095f478487ab8
Brian Tan <tanrui.brian@gmail.com>
2017-02-04 00:10:56 -0800

    Changed minimum filter bound to be inclusive

--- server/controllers/middleware/filters.js
+++ server/controllers/middleware/filters.js
@@ -40,11 +40,13 @@ function filters(req, res, next) {
     let comparison = '=';
 
     // Ending a query parameter with `Min` indicates that the parameter should
-    // be minimum the value it proides. For example,
-    // /events?startMin=2020-01-18 14:00:00 indicates that only the events
-    // after a certain date should be included.
+    // be minimum the value it provides. Minimum value bound is inclusive, and
+    // upper bound is exclusive.
+    //
+    // For example, /events?startMin=2020-01-18 14:00:00 indicates that only the
+    // events after a certain date should be included.
     if (endsWith(key, 'Min')) {
-      comparison = '>';
+      comparison = '>=';
       // Removes 'Min' from end of string to restore original attribute name.
       key = key.slice(0, -3);
     } else if (endsWith(key, 'Max')) {
